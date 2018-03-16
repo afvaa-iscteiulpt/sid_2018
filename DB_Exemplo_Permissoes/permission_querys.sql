eadd98bbb095b33902ab70c90b7d26c9c6650343
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2018-03-16 00:32:52 +0000

    Updated permissions query script

--- DB_Exemplo_Permissoes/permission_querys.sql
+++ DB_Exemplo_Permissoes/permission_querys.sql
@@ -1,26 +1,26 @@
-IF EXISTS (SELECT * FROM dbo.sysusers where dbo.sysusers.name = 'Investigadores')
-drop user "Investigadores";
+IF NOT EXISTS (SELECT * FROM dbo.sysusers where dbo.sysusers.name = 'Investigadores')
 create user "Investigadores";
 grant group to "Investigadores";
 
-IF EXISTS (SELECT * FROM dbo.sysusers where dbo.sysusers.name = 'Administradores')
-drop user "Administradores";
+IF NOT EXISTS (SELECT * FROM dbo.sysusers where dbo.sysusers.name = 'Administradores')
 create user "Administradores";
 grant group to "Administradores";
 
-IF EXISTS (SELECT * FROM dbo.sysusers where dbo.sysusers.name = 'Mongo')
-drop user "Mongo";
+IF NOT EXISTS (SELECT * FROM dbo.sysusers where dbo.sysusers.name = 'Mongo')
 create user "Mongo";
 
+// Permissões para Investigadores terao que ser atualizadas para implementar Triggers/SPs
+// Para limitar por cultura pretencente ao respetivo investigador
 revoke all from "Investigadores";
-grant insert, delete, update on DBA.VariaveisMedidas to "Investigadores";
-grant insert, delete, update on DBA.Medicoes to "Investigadores";
+grant select, update on DBA.Cultura to "Investigadores";
+grant select on DBA.VariaveisMedidas to "Investigadores"; 
+grant select, insert, delete, update on DBA.Medicoes to "Investigadores"; 
 
 revoke all from "Administradores";
 grant select on DBA.HumidadeTemperatura to "Administradores";
 grant select on DBA.Medicoes to "Administradores";
 grant select on DBA.Variaveis to "Administradores";
-grant select on DBA.VariaveisMedidas to "Administradores";
+grant select, insert, delete, update on DBA.VariaveisMedidas to "Administradores";
 grant select, insert, delete, update on DBA.Cultura to "Administradores";
 grant select, insert, delete, update on DBA.Investigador to "Administradores";
 
