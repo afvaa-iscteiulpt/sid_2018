3dfc5aa09e9b2cca97b84a06559f7d46985b7bce
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2018-03-18 23:55:18 +0000

    Updated sql permissions query

--- DB_Exemplo_Permissoes/permission_querys.sql
+++ DB_Exemplo_Permissoes/permission_querys.sql
@@ -1,3 +1,4 @@
+// Creating users and groups
 IF NOT EXISTS (SELECT * FROM dbo.sysusers where dbo.sysusers.name = 'Investigadores')
 create user "Investigadores";
 grant group to "Investigadores";
@@ -9,20 +10,31 @@ grant group to "Administradores";
 IF NOT EXISTS (SELECT * FROM dbo.sysusers where dbo.sysusers.name = 'Mongo')
 create user "Mongo";
 
-// Permissões para Investigadores terao que ser atualizadas para implementar Triggers/SPs
-// Para limitar por cultura pretencente ao respetivo investigador
+IF NOT EXISTS (SELECT * FROM dbo.sysusers where dbo.sysusers.name = 'SuperAdministrador')
+create user "SuperAdministrador";
+
+// Table permissions
 revoke all from "Investigadores";
-// Falta das grant aos views de select das tabelas Cultura, VariaveisMedidas e Medicoes
-grant update on DBA.Cultura to "Investigadores"; // Precisa de trigger before update
-grant insert, delete, update on DBA.Medicoes to "Investigadores";  // Precisa de trigger before update, delete, insert
+grant insert, update on DBA.Medicoes to "Investigadores";  // Precisa de trigger before update, delete, insert
 
 revoke all from "Administradores";
-grant select on DBA.HumidadeTemperatura to "Administradores";
-grant select on DBA.Medicoes to "Administradores";
-grant select on DBA.Variaveis to "Administradores";
-grant select, insert, delete, update on DBA.VariaveisMedidas to "Administradores";
-grant select, insert, delete, update on DBA.Cultura to "Administradores";
-grant select, insert, delete, update on DBA.Investigador to "Administradores";
+grant select, insert, delete, update on DBA.HumidadeTemperatura to "Administradores";
+grant select, insert, delete, update on DBA.Medicoes to "Administradores"; // SD SP
+grant select, insert, delete, update on DBA.Variaveis to "Administradores"; // SD SP
+grant select, insert, delete, update on DBA.VariaveisMedidas to "Administradores"; // SD SP
+grant select, insert, delete, update on DBA.Cultura to "Administradores"; // SD SP
+grant select, insert, delete, update on DBA.Investigador to "Administradores"; // SD SP
 
 revoke all from "Mongo";
 grant insert on DBA.HumidadeTemperatura to "Mongo";
+
+revoke all from "SuperAdministrador";
+/*
+Como dar permissão para adicionar users (investigadores e admins) à base de dados em codigo sql?
+*/
+
+/*
+Ver como dar permissão para criar usários e atribuir grupos
+*/
+
+// Stored Procedure Permissions
