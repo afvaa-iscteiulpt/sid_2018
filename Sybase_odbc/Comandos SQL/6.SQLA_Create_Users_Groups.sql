dbc9882ea48a362cc6ae94a14c863216f9bb77ab
rmnte-iscteiulpt <32777900+rmnte-iscteiulpt@users.noreply.github.com>
2018-04-12 23:04:08 +0100

    Updated permissions script

--- Sybase_odbc/Comandos SQL/6.SQLA_Create_Users_Groups.sql
+++ Sybase_odbc/Comandos SQL/6.SQLA_Create_Users_Groups.sql
@@ -3,6 +3,11 @@
 /*==============================================================*/
 
 /*
+OBSERVAÇÕES GERAIS
+Os SPs create/alter/delete users e admins estão comentados porque não estão implementados ainda
+*/
+
+/*
 INVESTIGADORES
 */
 if not exists (select * from dbo.sysusers where dbo.sysusers.name = 'Investigadores')
@@ -31,8 +36,8 @@ SUPERADMINISTRADOR
 if not exists (select * FROM dbo.sysusers where dbo.sysusers.name = 'SuperAdministrador')
 create user "SuperAdministrador" identified by "password";
 revoke all from "SuperAdministrador";
-// Dá erro caso ja seja membro. Como fazer: "if isn't member, then grant membership"?
-grant membership in group "Administradores" to "SuperAdministrador";
+// Está neste momento como utilizador separado, copiando os grants para os administradores
+//grant membership in group "Administradores" to "SuperAdministrador";
 
 /*==============================================================*/
 /* Grant Table Permissions                 						*/
@@ -54,6 +59,16 @@ grant select, insert, delete, update on DBA.Cultura to "Administradores";
 grant select on DBA.Investigador to "Administradores";
 
 /*
+SUPERADMINISTRADOR - COPIA DE ADMINISTRADORES
+*/
+grant select, insert, delete, update on DBA.HumidadeTemperatura to "SuperAdministrador";
+grant select, insert, delete, update on DBA.Medicoes to "SuperAdministrador";
+grant select, insert, delete, update on DBA.Variaveis to "SuperAdministrador";
+grant select, insert, delete, update on DBA.VariaveisMedidas to "SuperAdministrador";
+grant select, insert, delete, update on DBA.Cultura to "SuperAdministrador";
+grant select on DBA.Investigador to "SuperAdministrador";
+
+/*
 MONGODB
 */
 grant insert on DBA.HumidadeTemperatura to "MongoDB";
@@ -65,12 +80,11 @@ grant insert on DBA.HumidadeTemperatura to "MongoDB";
 /*
 INVESTIGADORES
 */
-// Por testar
-grant select on v_InvestigadorPorInvestigador to "Investigadores";
-grant select on v_CulturaPorInvestigador to "Investigadores";
-grant select on v_VariaveisPorInvestigador to "Investigadores";
-grant select on v_VariaveisMedidasPorInvestigador to "Investigadores";
-grant select on v_MedicoesPorInvestigador to "Investigadores";
+grant select on InvestigadorPorInvestigador to "Investigadores";
+grant select on CulturaPorInvestigador to "Investigadores";
+grant select on VariaveisPorInvestigador to "Investigadores";
+grant select on VariaveisMedidasPorInvestigador to "Investigadores";
+grant select on MedicoesPorInvestigador to "Investigadores";
 
 
 /*==============================================================*/
@@ -80,7 +94,6 @@ grant select on v_MedicoesPorInvestigador to "Investigadores";
 /*
 INVESTIGADORES
 */
-// O SP deve restringir o soft delete a culturas que nao sejam do investigador
 grant execute on SP_softDeleteMedicoes to "Investigadores";
 
 /*
@@ -89,21 +102,31 @@ ADMINISTRADORES
 grant execute on SP_softDeleteMedicoes to "Administradores";
 grant execute on SP_softDeleteVariaveisMedidas to "Administradores";
 grant execute on SP_softDeletevariaveis to "Administradores";
-grant execute on SP_softDeleteCulturas to "Administradores";
+grant execute on SP_softDeleteCultura to "Administradores";
 grant execute on SP_softDeleteInvestigador to "Administradores";
 
+/*
 grant execute on SP_CreateInvestigador to "Administradores";
 grant execute on SP_AlterInvestigador to "Administradores";
 grant execute on SP_DropInvestigador to "Administradores";
+*/
 
 /*
 SUPERADMINISTRADOR
 */
+/*
+grant execute on SP_CreateInvestigador to "SuperAdministrador";
+grant execute on SP_AlterInvestigador to "SuperAdministrador";
+grant execute on SP_DropInvestigador to "SuperAdministrador";
+
 grant execute on SP_CreateAdministrador to "SuperAdministrador";
 grant execute on SP_AlterAdministrador to "SuperAdministrador";
 grant execute on SP_DropAdministrador to "SuperAdministrador";
+*/
 
-
-
-
+grant execute on SP_softDeleteMedicoes to "SuperAdministrador";
+grant execute on SP_softDeleteVariaveisMedidas to "SuperAdministrador";
+grant execute on SP_softDeletevariaveis to "SuperAdministrador";
+grant execute on SP_softDeleteCultura to "SuperAdministrador";
+grant execute on SP_softDeleteInvestigador to "SuperAdministrador";
 
