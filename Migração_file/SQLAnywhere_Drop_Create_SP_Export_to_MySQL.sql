ae8a2442301ca4502a273276b28dcc148dbebdce
a16482-iscteiulpt <a16482@iscte-iul.pt>
2018-04-14 10:25:02 +0100

    sp export_to_mysql

---
+++ Migração_file/SQLAnywhere_Drop_Create_SP_Export_to_MySQL.sql
@@ -0,0 +1,32 @@
+﻿drop procedure if exists "DBA"."Export_to_MySQL";
+
+create procedure "DBA"."Export_to_MySQL"
+BEGIN
+ 	SELECT * FROM Log_HumidadeTemperatura;
+	OUTPUT TO Log_HumidadeTemperatura.txt
+    	FORMAT TEXT;
+	
+	SELECT * FROM Log_VariaveisMedidas;
+	OUTPUT TO Log_VariaveisMedidas.txt
+    	FORMAT TEXT;
+	
+	SELECT * FROM Log_Variaveis;
+	OUTPUT TO Log_Variaveis.txt
+    	FORMAT TEXT;
+	
+	SELECT * FROM Log_Cultura;
+	OUTPUT TO Log_Cultura.txt
+    	FORMAT TEXT;
+
+	SELECT * FROM Log_Investigadores;
+	OUTPUT TO Log_Investigadores.txt
+    	FORMAT TEXT;
+
+	SELECT * FROM Log_Medicoes;
+	OUTPUT TO Log_Medicoes.txt
+    	FORMAT TEXT;
+
+	SELECT * FROM Log_Consultas;
+	OUTPUT TO Log_Consultas.txt
+    	FORMAT TEXT;
+END;
